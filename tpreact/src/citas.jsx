c3168770c440daf2809cb56a75b9d5aa4f282fce
Frantag15 <48183956@ort>
2025-04-24 11:51:15 -0300

    citasjsx

---
+++ tpreact/src/citas.jsx
@@ -0,0 +1,16 @@
+import "./Cita.css";
+
+function Cita({ cita }) {
+  return (
+    <div className="cita">
+      <p><strong>Mascota:</strong> {cita.mascota}</p>
+      <p><strong>Dueño:</strong> {cita.dueño}</p>
+      <p><strong>Fecha:</strong> {cita.fecha}</p>
+      <p><strong>Hora:</strong> {cita.hora}</p>
+      <p><strong>Síntomas:</strong> {cita.sintomas}</p>
+      <button className="eliminar">Eliminar</button>
+    </div>
+  );
+}
+
+export default Cita;
