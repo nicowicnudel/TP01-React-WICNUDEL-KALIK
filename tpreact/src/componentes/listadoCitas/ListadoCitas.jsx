5ee372dcb30562f5e98e5512b0ce645737c84e1e
Frantag15 <48183956@ort>
2025-04-28 15:45:14 -0300

    terminamos listas

--- tpreact/src/componentes/listadoCitas/ListadoCitas.jsx
+++ tpreact/src/componentes/listadoCitas/ListadoCitas.jsx
@@ -4,9 +4,9 @@ import "./ListadoCitas.css";
 function ListadoCitas({ citas }) {
   return (
     <div className="listado">
-      <Cita mascota="tina" dueño = "chechu" fecha = "" hora = "" sintomas = "le duele la pata"></Cita> 
-      <Cita mascota="cacho" dueño = "nico" fecha = "" hora = "" sintomas = "le duele la panza"></Cita>  
-      <Cita mascota="ciro" dueño = "tomi" fecha = "" hora = "" sintomas = "le pica el pie izquierdo"></Cita>   
+      <Cita mascota="tina" dueño = "chechu" fecha = "2021-05-08" hora = "07:45" sintomas = "le duele la pata"></Cita> 
+      <Cita mascota="cacho" dueño = "nico" fecha = "2020-04-03" hora = "11:11" sintomas = "le duele la panza"></Cita>  
+      <Cita mascota="ciro" dueño = "tomi" fecha = "2025-02-01" hora = "20:20" sintomas = "le pica el pie izquierdo"></Cita>   
     </div>
   );
 }
